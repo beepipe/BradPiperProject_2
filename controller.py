afd710a5ad01a7b87357d5edea9d5b025b12c5d6
beepipe <bradleygpiper@gmail.com>
2022-12-09 12:45:47 -0600

    Second Upload

--- controller.py
+++ controller.py
@@ -15,9 +15,6 @@ class Controller(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         """
         This function initializes a controller with default values
-
-        :param args:
-        :param kwargs:
         """
         super().__init__(*args, **kwargs)
         self.setupUi(self)
@@ -44,6 +41,13 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.resetButton.clicked.connect(lambda: self.reset())
 
     def submit(self):
+        """
+        This function checks whether the user has entered anything into the 'nameField' entry box. If nothing is
+        entered, the ValueError is raised to indicate the user needs to enter something into that field in order
+        to access the start button and begin the program. If there is anything in the box, the startButton is enabled
+        and the errorText is hidden
+        :return:
+        """
         try:
             name = self.nameField.text()
             if name == '':
@@ -60,7 +64,11 @@ class Controller(QMainWindow, Ui_MainWindow):
 
     def start(self):
         """
-        This function will start the timer and increment by one second
+        This function will start the timer and disable the startButton upon pressing as well as the resetButton,
+        submitButton and nameField while enabling and setting the stopButton to unchecked as well as enabling the
+        splitButton. self.initiate is set to time.time() and self.initiate_split is established for capturing split
+        times utilizing the time library. Finally, the method self.convert is called to convert self.initiate to
+        readable time output.
         :return:
         """
         self.startButton.setEnabled(False)
@@ -69,59 +77,68 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.resetButton.setEnabled(False)
         self.splitButton.setEnabled(True)
         self.submitButton.setEnabled(False)
+        self.nameField.setEnabled(False)
         self.initiate = time.time()
-        self.initiate_split = time.time()
-        self.convert(self.initiate, self.preResetTotal, self.splitPreSetTotal)
+        self.initiate_split = time.time() - self.splitPreSetTotal
+        self.convert(self.initiate, self.preResetTotal)
 
     def stop(self):
         """
-        This function will stop the timer
+        This function will stop the timer and disable the startButton, stopButton, splitButton while reenabling the
+        resetButton to start a new timer and to write logged splits to a CSV file.
         :return:
         """
+
+        self.storeTotal = time.time() - self.initiate
+        self.loadTotal = self.storeTimes(self.storeTotal)
+        self.storeSplit = time.time() - self.initiate_split
+        self.loadSplit = self.storeTimes(self.storeSplit)
+        self.initiate_split = time.time()
+        self.splitPreSetTotal = time.time() - self.preResetTotal
+        self.splitCounter = self.splitCounter + 1
+        self.split_header.setText(f'Split {self.splitCounter}')
+        self.splits.append(f'Split {self.splitCounter}')
+        self.timeName.append(f'{self.username}')
+        self.totalTimes.append(f'{self.loadTotal}')
+        self.splitTimes.append(f'{self.loadSplit}')
+        self.convert(self.initiate_split, self.preResetTotal)
+
+        self.split_clock(self.splitPreSetTotal)
         self.stopButton.setEnabled(False)
         self.startButton.setChecked(False)
         self.startButton.setEnabled(True)
         self.resetButton.setEnabled(True)
         self.splitButton.setEnabled(False)
+        self.startButton.setEnabled(False)
+
+
 
     def split(self):
         """
-        This function will start the timer and increment by one second
+        This function will split the timer to capture splits in the total time upon clicking the splitButton
         :return:
         """
         self.storeTotal = time.time() - self.initiate
         self.loadTotal = self.storeTimes(self.storeTotal)
         self.storeSplit = time.time() - self.initiate_split
-        self.initiate_split = time.time()
         self.loadSplit = self.storeTimes(self.storeSplit)
-        self.splitPreSetTotal = 0.0
+        self.initiate_split = time.time()
+        self.splitPreSetTotal = time.time() - self.preResetTotal
         self.splitCounter = self.splitCounter + 1
         self.split_header.setText(f'Split {self.splitCounter}')
         self.splits.append(f'Split {self.splitCounter}')
         self.timeName.append(f'{self.username}')
         self.totalTimes.append(f'{self.loadTotal}')
         self.splitTimes.append(f'{self.loadSplit}')
-        self.convert(self.initiate_split, self.preResetTotal, self.splitPreSetTotal)
+        self.convert(self.initiate_split, self.preResetTotal)
 
     def reset(self):
         """
-        This function will reset the time to '00:00:00'
+        This function will reset all variables back to their default status and call the method save_times from writer
+        to write the logged times held in the lists timeName, splits, splitTimes and totalTimes to a CSV file
         :return:
         """
 
-        self.storeTotal = time.time() - self.initiate
-        self.loadTotal = self.storeTimes(self.storeTotal)
-        self.storeSplit = time.time() - self.initiate_split
-        self.initiate_split = time.time()
-        self.loadSplit = self.storeTimes(self.storeSplit)
-        self.splitPreSetTotal = 0.0
-        self.splitCounter = self.splitCounter + 1
-        self.split_header.setText(f'Split {self.splitCounter}')
-        self.splits.append(f'Split {self.splitCounter}')
-        self.timeName.append(f'{self.username}')
-        self.totalTimes.append(f'{self.loadTotal}')
-        self.splitTimes.append(f'{self.loadSplit}')
-        self.convert(self.initiate_split, self.preResetTotal, self.splitPreSetTotal)
         save_time(self.timeName, self.splits, self.splitTimes, self.totalTimes)
 
         self.time_display.setText('00:00:00:00')
@@ -137,6 +154,7 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.resetButton.setEnabled(False)
         self.startButton.setEnabled(False)
         self.submitButton.setEnabled(True)
+        self.nameField.setEnabled(True)
         self.nameField.setText('')
         self.username = ''
         self.splits.clear()
@@ -147,21 +165,33 @@ class Controller(QMainWindow, Ui_MainWindow):
 
 
 
-    def convert(self, start_time, total_time, split_time):
+    def convert(self, start_time, total_time):
+        """
+        This method will take in parameters to establish a time for the main time clock and split time clock in
+        time.time() format and then utilizes that conversion to call the methods main_clock and split_clock to display
+        time to the GUI
+        :param start_time: Time that the convert method was initially called by a button push
+        :param total_time: Total time held in the variable preResetTotal to keep track of total time
+        :return:
+        """
         while (self.startButton.isChecked() == True) and (self.stopButton.isChecked() == False):
             QtCore.QCoreApplication.processEvents()
             self.current_time = time.time()
             self.output = self.current_time - start_time + total_time
-            self.output_split = self.current_time - start_time + split_time
+            self.output_split = self.current_time - start_time
             self.main_clock(self.output)
             self.split_clock(self.output_split)
             self.adjust(self.output)
-
-        self.PreResetTotal = self.output
-        self.splitPreSetTotal = self.output_split
+            self.adjustSplit(self.output_split)
 
 
     def main_clock(self, display):
+        """
+        Method converting time.time() to traditional timeclock values to be displayed to the GUI via time_display.
+        Conversions are utilized to extract milliseconds, seconds, minutes and hours respectively.
+        :param display: The time.time() value to be converted to display
+        :return:
+        """
         milli = display * 100
         milli = milli % 100
         secs = display % 60
@@ -171,6 +201,12 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.time_display.setText(f'{int(hours):02}:{int(mins):02}:{int(secs):02}:{int(milli):02}')
 
     def split_clock(self, display_split):
+        """
+        Method converting time.time() to traditional timeclock values to be displayed to the GUI via
+        split_display. Conversions are utilized to extract milliseconds, seconds, minutes and hours respectively.
+        :param display: The time.time() value to be converted to display
+        :return:
+        """
         split_milli = display_split * 100
         split_milli = split_milli % 100
         split_secs = display_split % 60
@@ -180,6 +216,12 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.split_display.setText(f'{int(split_hours):02}:{int(split_mins):02}:{int(split_secs):02}:{int(split_milli):02}')
 
     def storeTimes(self, splitTimes):
+        """
+        Method converting time.time() to traditional timeclock values to be stored in respective lists and written out
+        to the CSV later
+        :param splitTimes: the time.time() value to be converted and returned to variable to later be stored in a list
+        :return: value returned to variable as readable time format for writing
+        """
         split_milli = splitTimes * 100
         split_milli = split_milli % 100
         split_secs = splitTimes % 60
@@ -189,7 +231,21 @@ class Controller(QMainWindow, Ui_MainWindow):
         return f'{int(split_hours):02}:{int(split_mins):02}:{int(split_secs):02}:{int(split_milli):02}'
 
     def adjust(self, newTime):
+        """
+        Method update PreResetTotal to newTime
+        :param newTime: New time Variable to load into PreResetTotal
+        :return:
+        """
         self.preResetTotal = newTime
+
+    def adjustSplit(self, newSplitTime):
+        """
+        Method updating splitPresetTotal to newSplitTime
+        :param newSplitTime: New time Variable to load into splitPreSetTotal
+        :return:
+        """
+        self.splitPreSetTotal = newSplitTime
+
 
 
 
--- writer.py
+++ writer.py
@@ -3,23 +3,40 @@ from os import path
 
 def save_time(name, splits, times, totals):
 
-    filename = str(name[0]) + '.csv'
+    filename = str(name[0]) + '_splittimelog.csv'
+
+    if path.exists(filename) == False:
 
 
-    with open(filename, 'a', newline='') as csvfile:
+        with open(filename, 'a', newline='') as csvfile:
 
 
-        fieldnames = ['Name', 'Splits', 'Times', 'Totals']
-        csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            fieldnames = ['Name', 'Splits', 'Times', 'Totals']
+            csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-        if path.exists(filename) == False:
+
             csvwriter.writeheader()
 
-        i = 0
+            i = 0
 
-        while i < len(splits):
-            csvwriter.writerow({'Name': name[i], 'Splits': splits[i], 'Times': times[i], 'Totals': totals[i]})
-            i = i + 1
+            while i < len(splits):
+                csvwriter.writerow({'Name': name[i], 'Splits': splits[i], 'Times': times[i], 'Totals': totals[i]})
+                i = i + 1
+
+    else:
+
+        with open(filename, 'a', newline='') as csvfile:
+
+
+            fieldnames = ['Name', 'Splits', 'Times', 'Totals']
+            csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            i = 0
+
+            while i < len(splits):
+                csvwriter.writerow({'Name': name[i], 'Splits': splits[i], 'Times': times[i], 'Totals': totals[i]})
+                i = i + 1
+
 
 
 
